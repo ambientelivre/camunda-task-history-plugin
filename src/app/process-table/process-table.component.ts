0ff9ac3759ad2700b3896cd169353beba45213bc
Your Name <sandrewtx08@pm.me>
2024-05-06 12:39:01 -0300

    feat: process table remove variable instance history

--- src/app/process-table/process-table.component.ts
+++ src/app/process-table/process-table.component.ts
@@ -1,9 +1,8 @@
 import { Component, Input, OnInit } from "@angular/core";
-import { Observable, from, map, merge, switchMap, toArray } from "rxjs";
+import { Observable, from, map, switchMap, toArray } from "rxjs";
 import { DetailService } from "../history/process-instance/detail/detail.service";
 import { History } from "../history/process-instance/history";
 import { TaskService } from "../history/process-instance/task/task.service";
-import { VariableService } from "../history/process-instance/variable/variable.service";
 
 @Component({
   selector: "custom-process-table[taskid]",
@@ -18,7 +17,6 @@ export class ProcessTableComponent implements OnInit {
 
   constructor(
     private taskService: TaskService,
-    private variableService: VariableService,
     private detailService: DetailService
   ) {}
 
@@ -27,26 +25,17 @@ export class ProcessTableComponent implements OnInit {
       .findOneTaskById(this.taskid)
       .pipe(
         switchMap(({ processInstanceId }) =>
-          merge(
-            this.detailService
-              .findManyProcessInstanceDetail({
-                processInstanceId,
-              })
-              .pipe(
-                switchMap((details) => from(details)),
-                map(History.fromDetail)
-              ),
-            this.variableService
-              .findManyVariableByTaskId({ processInstanceId })
-              .pipe(
-                switchMap((variable) => from(variable)),
-                map(History.fromVariable)
-              )
-          )
+          this.detailService
+            .findManyProcessInstanceDetail({
+              processInstanceId,
+            })
+            .pipe(
+              switchMap((details) => from(details)),
+              map(History.fromDetail)
+            )
         ),
         toArray(),
-        map(History.sortByStartTimeDesc
-        )
+        map(History.sortByStartTimeDesc)
       );
   }
 }
