4762b4fcb8b1da75a8821218e99b7dbfd6d2e9d0
Your Name <sandrewtx08@pm.me>
2024-05-08 20:12:44 -0300

    fix: sort column click mem leak

--- src/app/sort/sort-header.directive.ts
+++ src/app/sort/sort-header.directive.ts
@@ -3,20 +3,22 @@ import {
   ContentChildren,
   Directive,
   EventEmitter,
+  OnDestroy,
   Output,
   QueryList,
 } from "@angular/core";
-import { SortEvent } from "./sort-event";
+import { Subscription, from, mergeMap, tap } from "rxjs";
 import { SortColumnIconComponent } from "./sort-column-icon/sort-column-icon.component";
 import { SortColumnIdDirective } from "./sort-column-id.directive";
+import { SortEvent } from "./sort-event";
 
 @Directive({
   selector: "[customSortHeader]",
 })
-export class SortHeaderDirective implements AfterContentInit {
+export class SortHeaderDirective implements AfterContentInit, OnDestroy {
   id;
-  order;
   icon?: SortColumnIconComponent;
+  click: Subscription;
 
   @ContentChildren(SortColumnIdDirective)
   columnId!: QueryList<SortColumnIdDirective>;
@@ -25,30 +27,38 @@ export class SortHeaderDirective implements AfterContentInit {
   sortClick = new EventEmitter<SortEvent>();
 
   ngAfterContentInit(): void {
-    this.columnId.forEach((el) => {
-      el.click$.subscribe((id) => {
-        console.log(id);
+    this.click = from(this.columnId.toArray())
+      .pipe(
+        mergeMap((el) =>
+          el.click$.pipe(
+            tap((id) => {
+              if (this.icon) {
+                if (this.id !== id) {
+                  this.icon.order = undefined;
+                  this.icon = el.icon;
+                  this.icon.order = "desc";
+                } else if (this.icon.order === "desc") {
+                  this.icon.order = "asc";
+                } else if (this.icon.order === "asc") {
+                  this.icon.order = undefined;
+                } else {
+                  this.icon.order = "desc";
+                }
+              } else {
+                this.icon = el.icon;
+                this.icon.order = "desc";
+              }
 
-        if (this.icon) {
-          if (this.id !== id) {
-            this.icon.order = undefined;
-            this.order = "desc";
-          } else if (this.order === "desc") {
-            this.order = "asc";
-          } else if (this.order === "asc") {
-            this.order = undefined;
-          } else {
-            this.order = "desc";
-          }
-        } else {
-          this.order = "desc";
-        }
+              this.id = id;
+              this.sortClick.emit(new SortEvent(this.id, this.icon.order));
+            })
+          )
+        )
+      )
+      .subscribe();
+  }
 
-        this.icon = el.icon;
-        this.icon.order = this.order;
-        this.id = id;
-        this.sortClick.emit(new SortEvent(this.id, this.order));
-      });
-    });
+  ngOnDestroy(): void {
+    this.click.unsubscribe();
   }
 }
